3e2b84d99c3c65eff9e5c533808115e657340e78
pedroburthon-sys <pedro.burthon@ba.estudante.senai.br>
2026-02-11 16:58:29 -0300

    Add files via upload

---
+++ tipagem.py
@@ -0,0 +1,14 @@
+
+#  TIPAGEM DINÃMICA
+
+import os 
+
+os.system("cls")
+
+numero = int(input("digite um numero"))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("Antecessor:", antecessor)
+print("sucessor: ", sucessor)
